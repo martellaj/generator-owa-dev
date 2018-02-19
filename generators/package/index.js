98b0aa1e41ec270c9e42584f0003ec1d35287b2a
Joe Martella <martellaj@live.com>
2018-02-18 21:34:17 -0800

    Changes package subcommand to use a prompt to get package name
    Closes #8

--- generators/package/index.js
+++ generators/package/index.js
@@ -3,69 +3,75 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 module.exports = class extends Generator {
-    constructor(args, opts) {
-        super(args, opts);
-
-        this.argument('packageName', { type: String, required: true });
-    }
-
     package() {
-        const packageName = this.options.packageName;
+        this.prompt([
+            {
+                type    : 'input',
+                name    : 'name',
+                message : `What's your package's name?`,
+                validate: (value) => {
+                    if (value.length) {
+                        if (fs.existsSync(value)) {
+                            return 'A package with this name already exists at this location.';
+                        }
 
-        if (!fs.existsSync(packageName)) {
-            return this.prompt([{
-                type    : 'checkbox',
-                name    : 'subfolders',
-                message : 'Subfolders to create',
-                choices: [
-                    {
-                        name: 'actions'
-                    },
-                    {
-                        name: 'components'
-                    },
-                    {
-                        name: 'mutators'
-                    },
-                    {
-                        name: 'orchestrators'
-                    },
-                    {
-                        name: 'services'
-                    },
-                    {
-                        name: 'store'
-                    },
-                    {
-                        name: 'test'
-                    },
-                    {
-                        name: 'utils'
-                    },
-                    new inquirer.Separator('===== End of options =====')
-                ]
-            }]).then((answers) => {
-                const lib = 'lib';
+                        return true;
+                    } else {
+                        return `You can't create a package without a name. C'mon.`
+                    }
+                }
+            },
+            {
+            type    : 'checkbox',
+            name    : 'subfolders',
+            message : 'Subfolders to create',
+            choices: [
+                {
+                    name: 'actions'
+                },
+                {
+                    name: 'components'
+                },
+                {
+                    name: 'mutators'
+                },
+                {
+                    name: 'orchestrators'
+                },
+                {
+                    name: 'services'
+                },
+                {
+                    name: 'store'
+                },
+                {
+                    name: 'test'
+                },
+                {
+                    name: 'utils'
+                },
+                new inquirer.Separator('===== End of options =====')
+            ]
+        }]).then((answers) => {
+            const packageName = answers.name;
+            const lib = 'lib';
 
-                // Create package directory.
-                fs.mkdirSync(packageName);
+            // Create package directory.
+            fs.mkdirSync(packageName);
 
-                // Move into package folder and creat "lib" and then an "index.ts".
-                process.chdir(packageName);
-                fs.mkdirSync(lib);
-                process.chdir(lib);
-                fs.writeFileSync('index.ts', '');
+            // Move into package folder and creat "lib" and then an "index.ts".
+            process.chdir(packageName);
+            fs.mkdirSync(lib);
+            process.chdir(lib);
+            fs.writeFileSync('index.ts', '');
 
-                // Create subfolders that user wants.
-                answers.subfolders.forEach((subfolder => {
-                    fs.mkdirSync(subfolder);
-                }));
+            // Create subfolders that user wants.
+            answers.subfolders.forEach((subfolder => {
+                fs.mkdirSync(subfolder);
+            }));
 
-                // Let the user know we created the package.
-                this.log(`Successfully created the "${packageName}" package!`);
-            });
-        } else {
-            this.log('Unable to create package (one with the same name already exists at this location).');
-        }
+            // Let the user know we created the package.
+            this.log(`Successfully created the "${packageName}" package!`);
+        });
     }
 };
