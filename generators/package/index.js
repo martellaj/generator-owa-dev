f11070afdfc43dc9715e1f75dd1a25a0cd815792
Joe Martella <martellaj@live.com>
2018-02-18 21:13:23 -0800

    Adds ability to create skeleton folders in package subcommand
    - Closes #7

--- generators/package/index.js
+++ generators/package/index.js
@@ -1,5 +1,6 @@
 const Generator = require('yeoman-generator');
 const fs = require('fs');
+const inquirer = require('inquirer');
 
 module.exports = class extends Generator {
     constructor(args, opts) {
@@ -12,19 +13,57 @@ module.exports = class extends Generator {
         const packageName = this.options.packageName;
 
         if (!fs.existsSync(packageName)) {
-            const lib = 'lib';
+            return this.prompt([{
+                type    : 'checkbox',
+                name    : 'subfolders',
+                message : 'Subfolders to create',
+                choices: [
+                    {
+                        name: 'actions'
+                    },
+                    {
+                        name: 'components'
+                    },
+                    {
+                        name: 'mutators'
+                    },
+                    {
+                        name: 'orchestrators'
+                    },
+                    {
+                        name: 'services'
+                    },
+                    {
+                        name: 'store'
+                    },
+                    {
+                        name: 'test'
+                    },
+                    {
+                        name: 'utils'
+                    },
+                    new inquirer.Separator('===== End of options =====')
+                ]
+            }]).then((answers) => {
+                const lib = 'lib';
 
-            // Create package directory.
-            fs.mkdirSync(packageName);
+                // Create package directory.
+                fs.mkdirSync(packageName);
 
-            // Move into package folder and creat "lib" and then an "index.ts".
-            process.chdir(packageName);
-            fs.mkdirSync(lib);
-            process.chdir(lib);
-            fs.writeFileSync('index.ts', '');
+                // Move into package folder and creat "lib" and then an "index.ts".
+                process.chdir(packageName);
+                fs.mkdirSync(lib);
+                process.chdir(lib);
+                fs.writeFileSync('index.ts', '');
 
-            // Let the user know we created the package.
-            this.log(`Successfully created the "${packageName}" package!`);
+                // Create subfolders that user wants.
+                answers.subfolders.forEach((subfolder => {
+                    fs.mkdirSync(subfolder);
+                }));
+
+                // Let the user know we created the package.
+                this.log(`Successfully created the "${packageName}" package!`);
+            });
         } else {
             this.log('Unable to create package (one with the same name already exists at this location).');
         }
